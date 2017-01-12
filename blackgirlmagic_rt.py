758f7b1814c780c172a2163eb18bad20b3b2faea
Monica Powell <mmp2181@barnard.edu>
2017-01-11 22:49:38 -0500

    added terms and refactored code to consolidate search for list of terms

--- blackgirlmagic_rt.py
+++ blackgirlmagic_rt.py
@@ -4,30 +4,15 @@ from twython import Twython, TwythonError
 # create a Twython object by passing the necessary secret passwords
 twitter = Twython(config.api_key, config.api_secret, config.access_token, config.token_secret)
 
-# return tweets cotaining #BlackGirlMagic, mix of recent and popular tweets
-# response = twitter.search(q='"#BlackGirlMagic"  -filter:retweets', result_type="recent", count=100)
-response = twitter.search(q='"#BlackGirlMagic"  -filter:retweets', result_type="recent", count=2)
-#if r['retweet_count'] for r in responses['statuses']> 10:
-# [r['text'] for r in response['statuses'] if r['retweet_count'] > 1]
-try:
-    [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses']]
-    # [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses'] if tweet['retweet_count'] > 1]
-except TwythonError as e:
-    print(e)
+# return tweets containing #BlackGirlMagic or related terms, pulls the most recent tweets
 
 
-response = twitter.search(q='"#MelaninPoppin"  -filter:retweets', result_type="recent", count=2)
+terms = [ "#BlackGirlMagic", "#MelaninPoppin", "#BlackGirlsRock", "#BlackGirlsCode", "#CarefreeBlackGirl"]
+for term in terms:
 
-try:
-    [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses']]
-
-except TwythonError as e:
-    print(e)
-
-    response = twitter.search(q='"CarefreeBlackGirl"  -filter:retweets', result_type="recent", count=2)
+    response = twitter.search(q=term + '-filter:retweets', result_type="recent", count=2)
 
     try:
         [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses']]
-
     except TwythonError as e:
         print(e)
