714c58a12f8f30d7a1c809a520c459fd4e4d0895
Monica Powell <mmp2181@barnard.edu>
2017-01-11 21:55:44 -0500

    updated script to work in digital ocean droplet & include additional hashtags

--- blackgirlmagic_rt.py
+++ blackgirlmagic_rt.py
@@ -1,14 +1,33 @@
-
+import config
 from twython import Twython, TwythonError
 
 # create a Twython object by passing the necessary secret passwords
-twitter = twython.Twython(api_key, api_secret, access_token, token_secret)
+twitter = Twython(config.api_key, config.api_secret, config.access_token, config.token_secret)
 
 # return tweets cotaining #BlackGirlMagic, mix of recent and popular tweets
-response = twitter.search(q='"#BlackGirlMagic"  -filter:retweets', result_type="recent", count=100)
+# response = twitter.search(q='"#BlackGirlMagic"  -filter:retweets', result_type="recent", count=100)
+response = twitter.search(q='"#BlackGirlMagic"  -filter:retweets', result_type="recent", count=2)
 #if r['retweet_count'] for r in responses['statuses']> 10:
 # [r['text'] for r in response['statuses'] if r['retweet_count'] > 1]
 try:
-    [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses'] if tweet['retweet_count'] > 1]
+    [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses']]
+    # [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses'] if tweet['retweet_count'] > 1]
 except TwythonError as e:
     print(e)
+
+
+response = twitter.search(q='"#MelaninPoppin"  -filter:retweets', result_type="recent", count=2)
+
+try:
+    [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses']]
+
+except TwythonError as e:
+    print(e)
+
+    response = twitter.search(q='"CarefreeBlackGirl"  -filter:retweets', result_type="recent", count=2)
+
+    try:
+        [twitter.retweet(id = tweet["id_str"]) for tweet in response['statuses']]
+
+    except TwythonError as e:
+        print(e)
